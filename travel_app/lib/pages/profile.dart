fdf0d3541bd9f7622919453ccf42277ffdf27178
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-07-16 16:58:06 +0530

    refactor: Update profile page layout and add social media icons
    Refactor the profile page layout in the travel app to improve the visual design and user experience. Add social media icons for Facebook, Twitter, LinkedIn, and Google Plus. Update the avatar images and adjust the spacing between elements for better alignment.
    
    Recent user commits:
    - feat: Add map page to the travel app
    - feat: Add cart page to the travel app
    
    Recent repository commits:
    - feat: Add Onboarding page to the travel app
    - chore: Add intro page 3 to the travel app
    - chore: Add intro page 2 to the travel app
    - refactor: CustomInputField components, remove unused code and comments
    - chore: Remove unused code and comments, refactor QrGenerator and CustomInputField components
    - refactor: Replace SideMenu component with NavigationBar component
    - chore: Add video player and device preview dependencies

--- travel_app/lib/pages/profile.dart
+++ travel_app/lib/pages/profile.dart
@@ -15,33 +15,147 @@ class _ProfileState extends State<Profile> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Template(
-      screenWidth: screenWidth,
-      screenHeight: screenHeight,
-      theChild: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: screenHeight * 0.2),
-            const Text(
-              "Sing in now",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
+        screenWidth: screenWidth,
+        screenHeight: screenHeight,
+        theChild: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Container(
+            color: Color.fromARGB(0, 255, 255, 255),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 105,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 65,
+                      backgroundImage: AssetImage("assets/6195145.jpg"),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage("assets/download.png"),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    CircleAvatar(
+                      backgroundImage:
+                          AssetImage("assets/GooglePlus-logo-red.png"),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    CircleAvatar(
+                      backgroundImage: AssetImage(
+                          "assets/1_Twitter-new-icon-mobile-app.jpg"),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    CircleAvatar(
+                      backgroundImage:
+                          AssetImage("assets/600px-LinkedIn_logo_initials.png"),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Thomas Shelby",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 26),
+                    )
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("@peakyBlinders")],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Master manipulator, deal-maker and\n                   entrepreneur",
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  child: Expanded(
+                      child: ListView(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        color: Colors.white70,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          leading: Icon(
+                            Icons.privacy_tip_sharp,
+                            color: Colors.black54,
+                          ),
+                          title: Text(
+                            'Privacy',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        color: Colors.white70,
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          leading: Icon(
+                            Icons.logout,
+                            color: Colors.black54,
+                          ),
+                          title: Text(
+                            'Logout',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(Icons.arrow_forward_ios_outlined),
+                        ),
+                      )
+                    ],
+                  )),
+                )
+              ],
             ),
-            SizedBox(height: screenHeight * 0.003),
-            const Text(
-              "Please sing in to continue our app",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: screenHeight * 0.04),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
